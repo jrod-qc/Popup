5625c0bf433d2c15d0d0b539b70f4a6273391bc2
Juan AKA JRod <juan.rodriguez@qc-mfg.com>
2021-05-14 11:15:23 -0700

    This is the files for the Popup

---
+++ script.js
@@ -0,0 +1,8 @@
+ // This is the Rebate Modal
+ setTimeout(function() {
+     $(".rebate-modal").css("display", "flex");
+ }, 5000)
+
+ $(".exit-btn").click(function() {
+     $(".rebate-modal").css("display", "none");
+ });
